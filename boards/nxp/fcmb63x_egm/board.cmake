64a5d1fd169dda5077767a2a83c4bcfe03c8f3d2
Robert Eichinger <robert.eichinger@bshg.com>
2026-02-05 09:01:42 +0100

    boards: nxp: Add support for fcmb63x_egm
    Add basic board definition for the M3uC development board (B sample)
    provided by NXP.

---
+++ boards/nxp/fcmb63x_egm/board.cmake
@@ -0,0 +1,14 @@
+# Copyright 2022-2023 NXP
+# SPDX-License-Identifier: Apache-2.0
+
+board_runner_args(jlink "--device=FCMB63X" "--reset-after-load")
+
+board_runner_args(linkserver  "--device=RW612:FRDM-RW612")
+
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/linkserver.board.cmake)
+
+if(CONFIG_BUILD_WITH_TFM)
+  # Flash merged TF-M + Zephyr binary
+  set_property(TARGET runners_yaml_props_target PROPERTY hex_file tfm_merged.hex)
+endif()
